0338050da858a8f5d7e1d65a2ee661d6dbd88a29
Emil Arran Baleva <arranbaleva@gmail.com>
2018-03-11 16:31:56 +0800

    Working registration, login, logout.

--- channelshowdown/userprofile/urls.py
+++ channelshowdown/userprofile/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
-from .views import RegistrationView
+from .views import RegistrationView, LoginView, LogoutView
 
 urlpatterns = [
     url(r'^registration/$', RegistrationView.as_view(), name='registration'),
+    url(r'^login/$', LoginView.as_view(), name='login'),
+    url(r'^logout/$', LogoutView.as_view(), name='logout'),
 ]
--- channelshowdown/userprofile/views.py
+++ channelshowdown/userprofile/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
 from django.views.generic import View
-from django.http import HttpResponse, JsonResponse
+from django.http import HttpResponse, JsonResponse, HttpResponseNotFound
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.models import User
 from .models import UserInfo
@@ -15,12 +15,13 @@ from .models import UserInfo
 @method_decorator(csrf_exempt, name='dispatch')
 class RegistrationView(View):
     def post(self, request, **kwargs):
-        username = request.POST['username']
-        password = request.POST['password']
-        email = request.POST['email']
+        username = request.POST.get('username', None)
+        password = request.POST.get('password', None)
+        email = request.POST.get('email', None)
+        # password = request.POST['password']
+        # email = request.POST['email']
         # User.objects.create(username=username, password=password, email=email)
         # user = authenticate(request, username=username, password=password)
-        import pdb; pdb.set_trace()
         user, created = User.objects.get_or_create(username=username)
         context = {
             'username': username,
@@ -29,13 +30,43 @@ class RegistrationView(View):
         }
         if created:
             user.set_password(password)
+            user.email = email
             user.save()
-            if request.POST['userType'] == "normal":
+            context['user_id'] = user.id
+            if request.POST.get('userType', None) == "normal":
                 userinfo = UserInfo(user_id=user.id, user_type="normal")
                 userinfo.save()
-            # login(request, user)
+            elif request.POST.get('userType', None) == "commentator":
+                userinfo = UserInfo(user_id=user.id, user_type="commentator")
+                userinfo.save()
             context['status'] = "registered"
             return JsonResponse(context)
         else:
             context['status'] = "not registered"
             return JsonResponse(context)
+
+
+@method_decorator(csrf_exempt, name='dispatch')
+class LoginView(View):
+    def post(self, request, **kwargs):
+        username = request.POST.get('username', None)
+        password = request.POST.get('password', None)
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            userinfo = UserInfo.objects.get(user=user.id)
+            context = {
+                'username': user.username,
+                'email': user.email,
+                'userType': userinfo.user_type,
+            }
+            login(request, user)
+            return JsonResponse(context)
+        else:
+            return HttpResponseNotFound("login failed")
+
+
+@method_decorator(csrf_exempt, name='dispatch')
+class LogoutView(View):
+    def post(self, request, **kwargs):
+        logout(request)
+        HttpRespose("logged out")
